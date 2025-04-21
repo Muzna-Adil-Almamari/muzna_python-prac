343a5bcaebf765a4b94b2e2f88b4f58c7f062e17
Muzna-Adil-Almamari <muznaadilalmamari@gmail.com>
2025-04-21 12:25:59 +0400

    add logical operators in calc.py

--- calc.py
+++ calc.py
@@ -3,32 +3,44 @@ import math
 
 def cal():
     num1=int(input("Enter first number: "))
-    operation= input("Select an operation (+, -, *, /, **, ! , |):")
+    operation= input("Select an operation (+, -, *, /, **, !, |, or, and, xor):")
     
 
     if operation =="!":
-        res = math.factorial(num1)
-        print (f"the result = {res}")
-    elif operation == "|":
-        res = abs(num1)
+        res = 1
+        for i in range(1, num1 + 1):
+            res *= i
         print(f"The result = {res}")
+
+    elif operation == "|":
+        def absolute_value(n):
+            return -n if n < 0 else n
+        res = absolute_value(num1)
+        print(f"The result = {res}")
+
     else:
-        num2=int(input("Enter scond number: "))
+        num2=int(input("Enter second number: "))
         if operation == "+":
             res= num1 + num2
         elif operation == "-":
             res= num1 - num2
         elif operation == "*":
             res= num1 * num2
-        elif operation == "/ ":
+        elif operation == "/":
             res= num1 / num2
         elif operation == "**":
             res= (num1 ** num2)
+        elif operation == "or":
+            res = num1 | num2
+        elif operation == "and":
+            res = num1 & num2
+        elif operation == "xor":
+            res = num1 ^ num2
         else:
             print("Invalid operation.")
             return
         
-        print (f"the result = {res}")
+        print (f"The result = {res}")
     
 
 while True:
