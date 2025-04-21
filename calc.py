66394730c7d24a1ae78fcfd03c3eee9c6fd73a32
Muzna-Adil-Almamari <muznaadilalmamari@gmail.com>
2025-04-21 10:07:36 +0400

    modified ! file calc.py

--- calc.py
+++ calc.py
@@ -1,19 +1,31 @@
+import math
+
+
 def cal():
     num1=int(input("Enter first number: "))
-    opration= input("select any opration ")
-    num2=int(input("Enter scond number: "))
+    opration= input("Select an operation (+, -, *, /, **, !):")
     
-    if opration == "+":
-        res= num1 + num2
-    elif opration == "-":
-        res= num1 - num2
-    elif opration == "*":
-        res= num1 * num2
-    elif opration == "/":
-        res= num1 / num2
-    elif opration == "**":
-        res= (num1 ** num2)
-    print (f"the result = {res}")
+
+    if opration =="!":
+        res = math.factorial(num1)
+        print (f"the result = {res}")
+    else:
+        num2=int(input("Enter scond number: "))
+        if opration == "+":
+            res= num1 + num2
+        elif opration == "-":
+            res= num1 - num2
+        elif opration == "*":
+            res= num1 * num2
+        elif opration == "/ ":
+            res= num1 / num2
+        elif opration == "**":
+            res= (num1 ** num2)
+        else:
+            print("Invalid operation.")
+            return
+        
+        print (f"the result = {res}")
     
 
 while True:
