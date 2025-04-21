81e0f2c7b882b62bf18002c30c9101c5066bab37
Muzna-Adil-Almamari <muznaadilalmamari@gmail.com>
2025-04-21 09:52:47 +0400

    Now file calc.py

---
+++ calc.py
@@ -0,0 +1,19 @@
+def cal():
+    num1=int(input("Enter first number: "))
+    opration= input("select any opration ")
+    num2=int(input("Enter scond number: "))
+    
+    if opration == "+":
+        res= num1 + num2
+    elif opration == "-":
+        res= num1 - num2
+    elif opration == "*":
+        res= num1 * num2
+    elif opration == "/":
+        res= num1 / num2
+    print (f"the result = {res}")
+    
+while True:
+    cal()
+
+ 
